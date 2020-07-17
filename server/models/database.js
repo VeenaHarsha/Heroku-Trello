3ecb01785842e9294068437ce22a31f13d13cbd7
VeenaHarsha <veena.achyuth@gmail.com>
2020-07-17 19:15:31 +0530

    changes added

--- server/models/database.js
+++ server/models/database.js
@@ -3,15 +3,15 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, 
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 })
 
-const query = async (sql, params) => {
-  try {
-    return await pool.query(sql, params)
-  } catch (err) {
-    console.log(err)
-  }
-}
-module.exports = { query }
+// const query = async (sql, params) => {
+//   try {
+//     return await pool.query(sql, params)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
+module.exports = pool
